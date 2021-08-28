84f7816b92617dfa020c415c739e09670d90bd13
Liosvaldo Santiago <liosvaldosantiago@gmail.com>
2021-08-27 23:24:07 -0300

    create file for test with operations that count find and any

--- src/main/kotlin/one/digitalinovation/collections/TestOperacoes.kt
+++ src/main/kotlin/one/digitalinovation/collections/TestOperacoes.kt
@@ -12,5 +12,18 @@ fun main(){
     println("_____________________")
     val salarioMaiorQue = salarios.filter { it >=2500.00 }
     salarioMaiorQue.forEach{println(it)}
+
+    println("___________________")
+    println(salarios.count{it in 2000.0 .. 5000.00})
+
+    println("___________________")
+    println(salarios.find{it == 2500.0})
+
+    println("___________________")
+    println(salarios.find{it == 2550.0})
+
+    println("___________________")
+    println(salarios.any{it == 1000.0})
+
 }
 
