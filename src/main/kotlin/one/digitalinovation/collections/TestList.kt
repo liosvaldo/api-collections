8d92709e9214630b4b7c6f44de23f4b1678e2bd6
Liosvaldo Santiago <liosvaldosantiago@gmail.com>
2021-08-27 23:41:23 -0300

    append in the TestList the test with groupBy

--- src/main/kotlin/one/digitalinovation/collections/TestList.kt
+++ src/main/kotlin/one/digitalinovation/collections/TestList.kt
@@ -2,11 +2,11 @@ package one.digitalinovation.collections
 
 fun main(){
 
-    val joao = Funcionario("joao", 5000.00)
+    val joao = Funcionario("joao", 5000.00, "CLT")
 
-    val pedro = Funcionario("pedro", 2000.00)
+    val pedro = Funcionario("pedro", 2000.00, "PJ")
 
-    val maria = Funcionario("maria", 4000.00)
+    val maria = Funcionario("maria", 4000.00, "CLT")
 
     val funcionarios = listOf(joao, pedro, maria)
     funcionarios.forEach { println(it) }
@@ -15,12 +15,18 @@ fun main(){
 
     println("_____________________")
     funcionarios.sortedBy { it.salario }.forEach{println(it)}
+
+    println("_____________________")
+    funcionarios
+        .groupBy { it.tipoContratacao }
+        .forEach{println(it)}
 }
 
 data class Funcionario(
 
     val nome: String,
-    val salario: Double
+    val salario: Double,
+    val tipoContratacao: String
 
 ){
     override fun toString(): String =
